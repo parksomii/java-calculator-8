44a1176cd8ffffef0b1dd149748aa31d205a8db0
parksomii <somi020605@gmail.com>
2025-10-18 19:29:54 +0900

    feat: ErrorView 구현
    - 에러 메시지를 출력하는 뷰 클래스 추가

---
+++ src/main/java/calculator/view/ErrorView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+public class ErrorView {
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+
+    private ErrorView() {
+        // 유틸리티 클래스
+    }
+}
+
